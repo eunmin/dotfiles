62478e9b856969ee76738bbcccf5daa03960c912
Eunmin Kim <telepopsound@gmail.com>
2015-12-01 18:26:08 +0900

    squiggly-clojure 설정 추가

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,7 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-       		  [refactor-nrepl "2.0.0-SNAPSHOT"]]
+       		  [refactor-nrepl "2.0.0-SNAPSHOT"]
+                  [lein-environ "1.0.0"]]
 	:dependencies [[acyclic/squiggly-clojure "0.1.4"]
-                       [org.clojure/tools.nrepl "0.2.12"]]}}
+                       [org.clojure/tools.nrepl "0.2.12"]]}
+ :env {:squiggly {:checkers [:eastwood :typed :kbit]
+                  :eastwood-exclude-linters [:unlimited-use]}}}
