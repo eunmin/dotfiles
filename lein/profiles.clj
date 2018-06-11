df03cd7695f0fdfe07368e1a05662031f71db850
Eunmin Kim <telepopsound@gmail.com>
2018-06-11 13:46:18 +0900

    lein 제거

--- lein/profiles.clj
+++
@@ -1,8 +0,0 @@
-{:init {:aether #=(eval (do (require 'cemerick.pomegranate.aether)
-                            (cemerick.pomegranate.aether/register-wagon-factory!
-                             "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))))}
- :user {:plugin-repositories [["mail-release" {:url "http://maven.daumcorp.com/content/repositories/dk-mailcloud-release"
-                                               :update :always}]
-                              ["mail-snapshot" {:url "http://maven.daumcorp.com/content/repositories/dk-mailcloud-snapshots"
-                                                :update :always}]]
-        :plugins [[lein-try "0.4.3"]]}}
